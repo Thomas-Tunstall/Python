febff804c962e06b24ae59b6c7ecbc4eb1eef707
plaetzaw <plaetzaw@gmail.com>
2020-01-14 16:03:50 -0600

    today

---
+++ Tues1.20.py
@@ -0,0 +1,60 @@
+# name = input('What is your name?')
+# print('Hello', (name))
+
+
+# name = input("What is your name?")
+# length_of_name = len(name)
+# if length_of_name > 0:
+#     print('HELLO' + name.upper())
+#     print('YOUR NAME HAS') + (str(length_of_name) + ('LETTERS! AWESOME!')
+
+
+# print("Fill in the Blanks Below")
+# AName = input('What is your name? ')
+# ASubj = input('What is your favorite subject? ')
+# AFood = input('What is your favorite food? ')
+
+
+# print(f"{AName}'s favorite subject in school is {ASubj}. They also enjoy to eat {AFood} during lunch.")
+
+# day = int(input('Day (0-6)? '))
+# if day == 0:
+#     print('Sunday')
+# elif day == 1:
+#     print('Monday')
+# elif day == 2:
+#     print('Tuesday')
+# elif day == 3:
+#     print('Wednesday')
+# elif day == 4:
+#     print('Thursday')
+# elif day == 5:
+#     print('Friday')
+# elif day == 6:
+#     print('Saturday')
+# else:
+#     print('Invalid #')
+
+# day = int(input('Day (0-6)? '))
+# if day == 0:
+#     print('Sleep In')
+# elif day == 1:
+#     print('Go to Work!')
+# elif day == 2:
+#     print('Go to Work!')
+# elif day == 3:
+#     print('Go to Work!')
+# elif day == 4:
+#     print('Go to Work!')
+# elif day == 5:
+#     print('Go to Work!')
+# elif day == 6:
+#     print('Sleep In')
+# else:
+#     print('Invalid #')
+
+
+"""Did't quite Fin"""
+temp = input('Enter your temperature here')
+convert_to_F = (temp * 1.8 + 32)
+print(convert_to_F)
