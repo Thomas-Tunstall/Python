a74dbe1ac3b5c0a6b927719f8d4d795dca28f7ba
plaetzaw <plaetzaw@gmail.com>
2020-01-15 15:58:03 -0600

    End of class 1.21

--- Wed1.21.py
+++ Wed1.21.py
@@ -1,15 +1,55 @@
-#1 Multiply Vectors
+# #1 Multiply Vectors
 
-listA = [2, 4, 5]
-listB = [2, 3, 6]
-listC = []
+# listA = [2, 4, 5]
+# listB = [2, 3, 6]
+# listC = []
 
-listC = [a*b for a,b in zip(listA,listB)]
+# listC = [a*b for a,b in zip(listA,listB)]
 
-print(listC)
+# print(listC)
 
 
 #2 Matrix Addition
 
-listA = [2, -2]
-listB = [5, 3]
+# listA = [ [1, 2], [3, 4]]
+# listB = [ [1, 2], [3, 4]]
+# listC = [[0, 0], [0, 0]]
+
+# for i in range (len(listA)):
+#     for l in range (len(listA[0])):
+#         listC[i][l] = listA[i][l] + listB[i][l]
+# print(listC)
+
+
+
+# #3 Matrix Addition II
+# listA = [ [1, 2, 3], [3, 4, 5]]
+# listB = [ [1, 2, 3], [3, 4, 5]]
+# listC = [[0, 0, 0], [0, 0, 0]]
+
+# for i in range (len(listA)):
+#     for l in range (len(listA[0])):
+#             listC[i][l] = listA[i][l] + listB[i][l]
+            
+# print(listC)
+
+
+# 4 De-dup
+# a = [1,1,1,3,3,4,4,4,5,5,6]
+# b = []
+
+# b = list(dict.fromkeys(a))
+# # print(a)
+# print(b)
+
+# 5 Leetspeak
+
+replacements = (('a', '4'), ('e', '3',), ('g', '6'), ('i', '1'), ('o', '0'), ('s', '5'), ('t', '7'))
+for leet in replacements:
+    if
+
+# # 6 Long-long Vowels
+
+# vowels = (('a' = 'aaaaa'), ('e', 'eeeee',), ('i', 'iiiii'), ('o', 'ooooo'), ('u', 'uuuuuu'))
+# long = vowels.replace(a,b)
+# print(long('cheese'))
