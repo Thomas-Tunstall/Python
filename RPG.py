a9c57a428a969c76ae7b7c37d18d1b2b4caf0aec
plaetzaw <plaetzaw@gmail.com>
2020-01-21 12:50:42 -0600

    Step 1 Complete

---
+++ RPG.py
@@ -0,0 +1,11 @@
+class Hero:                                             #Hero Class
+    def __init__(self, hero_health, hero_power):
+        self.hero_health = hero_health
+        self.hero_power = hero_power
+
+class Goblin:                                           #Goblin Class
+    def __init__(self, goblin_health, goblin_power):
+        self.goblin_health = goblin_health
+        self.goblin_power = goblin_power
+
+
