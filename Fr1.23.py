0e7d1aa784dc269f6171e4bc9b1dee9a6762c98e
plaetzaw <plaetzaw@gmail.com>
2020-01-21 10:09:24 -0600

    After HW review

--- Fr1.23.py
+++ Fr1.23.py
@@ -20,51 +20,96 @@
 # Please enter a word: banana
 # {'a': 3, 'b': 1, 'n': 2}
 
-your_string = "Make a string any string "
-HoldingCell = {}
+# your_string = input("Make a string any string ")
+# HoldingCell = {}
 
-for keys in your_string:
-    HoldingCell[keys] = HoldingCell.get(keys, 0) + 1
+# for keys in your_string:
+#     HoldingCell[keys] = HoldingCell.get(keys, 0) + 1
 
-print ("Here's the count" + str(HoldingCell))
+# print ("Here's the count" + str(HoldingCell))
 
+
+"""Another Example"""
+# word = input("Word here")
+# empty_dict = {}
+
+# for letters in word:
+#     if letters not in empty_dict:
+#         empty_dict[letters] = 0
+#     empty_dict[letters] += 1
+# print (empty_dict)
+
+"""
+for char in word:
+    if char not in dict:
+        dict[char] = 0
+    dict + char += 1     
+
+"""
+
+"""" for char in word:
+   if char not in dict
+       dict[char] = 1
+   else dict + char = dict + [char] + 1"""
 
 """Med #2"""
 # count = {}
 
-# example = "I like cake"
+# example = input("Put a string here")
 
 # for i in example.split():
 #     count[i] = len(i)
 
-# print(count)
-
-# myDict = {}
-# word = print(input(str("Pick a word any word")))
-
-# for i in word.split():
-#     count[i] = len(i)
-
-# print(myDict)       
+# print(count)    
 
 """Med 3"""
-# 3. Sorting a histogram
+"""# 3. Sorting a histogram
 # Please enter a sentence: To be or not to be do be do be do
 # The top three words are:
 # be: 4
 # do: 3
-# to: 2
+# to: 2"""
 
-your_string = "Make a string any string "
-HoldingCell = {}
+# your_string = "Make a string any string "
+# HoldingCell = {}
 
-for keys in your_string:
-    HoldingCell[keys] = HoldingCell.get(keys, 0) + 1
-del HoldingCell[" "]
+# for keys in your_string:
+#     HoldingCell[keys] = HoldingCell.get(keys, 0) + 1
+# del HoldingCell[" "]
 
-for key, value in sorted(HoldingCell.items()):
-    print ("Here's the count  " + str(HoldingCell))
+# for key, value in sorted(HoldingCell.items()):
+#     if len(keys) > 3:
+#         HoldingCell.append(keys, value)
+#     elif len(keys) = 3:
+#         print(HoldingCell)
 
-for keys in HoldingCell(keys, 0):
-    if len(keys) > 3:
-        append.HoldingCell(keys, value)
+
+LC = {}
+word = input(‘Please enter a word to count top 3 letters: ’)
+# word=‘hellllooooooohahahhhhh’
+for l in word:
+    if l in LC:
+        LC[l] = LC[l]+1
+    else:
+        LC[l] = 1
+top_list=[[“”,0],[“”,0],[“”,0]]
+for k, v in LC.items():
+    if v > top_list[0][1]:
+        top_list[2][0]=top_list[1][0]
+        top_list[2][1]=top_list[1][1]
+        top_list[1][0]=top_list[0][0]
+        top_list[1][1]=top_list[0][1]
+        top_list[0][0]=k
+        top_list[0][1]=v
+    elif v > top_list[1][1]:
+        top_list[2][0]=top_list[1][0]
+        top_list[2][1]=top_list[1][1]
+        top_list[1][0]=k
+        top_list[1][1]=v
+    elif v > top_list[2][1]:
+        top_list[2][0]=k
+        top_list[2][1]=v
+print(top_list)
+
+
+# print ("Here's the count  " + str(HoldingCell))
